751258fadaa77a3e55c3319fda4774a4cbd85519
Эмир Усеинов <emir15useinov@yandex.ru>
2023-11-08 23:34:10 +0300

    diplom

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,10 +76,6 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT')
     }
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
 }
 
 AUTH_USER_MODEL = 'users.User'
